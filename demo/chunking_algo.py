49809430a253f26390d085e52b064b7036205e38
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2020-09-20 16:07:48 +0800

    Add files via upload

--- demo/chunking_algo.py
+++ demo/chunking_algo.py
@@ -14,7 +14,6 @@ def buildMap(sequence, chunk):
         ptr += chunk
     return Map
 
-
 def buildCombination(pattern, chunk):
     #expect n-(c-1) time complexity; n - pattern length, c - chunk size
     m = len(pattern)
@@ -29,28 +28,34 @@ def buildCombination(pattern, chunk):
         ptr += 1
     return Map
 
-
 def checkPattern(sequence, pattern, sequenceLoc, patternLoc, chunk):
+    # expect n - c time complexity
+    sequenceLength = len(sequence)
     #check front of chunk
     for x in range(0,patternLoc):
-        if (pattern[x] != sequence[sequenceLoc + (x - patternLoc)]) : return False # + 10000000
+        sequenceIndex = sequenceLoc + (x - patternLoc) + sequence.index[0]
+        OutofBound = ( False if 0 > sequenceIndex or sequenceIndex > sequenceLength else True)
+        if (OutofBound and pattern[x] != sequence[sequenceIndex]) : return False
     #check back of chunk
     for x in range(patternLoc + chunk,len(pattern)):
-        if (pattern[x] != sequence[sequenceLoc + (x - patternLoc)]) : return False # + 10000000
+        sequenceIndex = sequenceLoc + (x - patternLoc) + sequence.index[0]
+        OutofBound = ( False if 0 > sequenceIndex or sequenceIndex > sequenceLength else True)
+        if (OutofBound and pattern[x] != sequence[sequenceIndex]) : return False
     return True
 
 
 def chunkingAlgo(sequence, pattern):
     start_time = time.time()
+
     # chunk const has to be less then half of the length of sequence to ensure pattern will definitely hit in the chucking of sequence.
     chunk = int(len(pattern) / 2)
     sequenceSet = buildMap(sequence, chunk)
-    patternSet = buildCombination(pattern, chunk)
-
-    #print(patternSet)
-    #print(sequenceSet)
 
     print("--- Pre-Processing %s seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+
+    patternSet = buildCombination(pattern, chunk)
 
     start_time = time.time()
 
@@ -60,7 +65,7 @@ def chunkingAlgo(sequence, pattern):
             for sequenceElement in sequenceSet[keyPattern]:
                 for patternElement in valuePattern:
                     if checkPattern(sequence, pattern, sequenceElement, patternElement, chunk):
-                        indexlist.append(sequenceElement - patternElement)
+                        indexlist.append(sequenceElement - patternElement + sequence.index[0])
 
     print("--- Processing %s seconds ---" % (time.time() - start_time))
 
